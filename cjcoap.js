da99eda5baf5a2ea7f474a197892f9d4d4427242
ikpark <ikpark@gmail.com>
2015-02-24 10:03:40 +0900

    add sending response (buggy)

--- cjcoap.js
+++ cjcoap.js
@@ -2,6 +2,7 @@ var udp = require('./udp');
 var resources = require('./resources');
 var connections = require('./connections');
 var Message = require('./message');
+var cli_socket, svr_socket;
 
 function handle_resource(peer, msg) {
 
@@ -9,9 +10,41 @@ function handle_resource(peer, msg) {
   var r = resources.get(resource_uri);
 
   if(r) {
+    // TODO: r.handle[msg.code]()
+    //    -> default_handler()    : handle ETag, valid time
+    //    -> handle_test_get()    : user-defined function
     var ret = r.handle[msg.code]();
-    if(ret) console.log(ret);
+    if(ret) {
+      // XXX
+      msg.type = 'ACK';
+      msg.code = ret.code;
+      msg.options = [];
+      if(ret.data) {
+        msg.payload = ret.data;
+
+        var o = {};
+        var v = new Buffer(1);
+        o.type = 'Content-Format';
+        if(ret.encoding.search('text') != -1)
+          v.writeUInt8(0, 0);
+        else if(ret.encoding.search('link-format') != -1)
+          v.writeUInt8(40, 0);
+        else if(ret.encoding.search('xml') != -1)
+          v.writeUInt8(41, 0);
+        else if(ret.encoding.search('octet-stream') != -1)
+          v.writeUInt8(42, 0);
+        else if(ret.encoding.search('exi') != -1)
+          v.writeUInt8(47, 0);
+        else if(ret.encoding.search('json') != -1)
+          v.writeUInt8(50, 0);
+
+        o.value = new Buffer(v);
+        msg.options.push(o);
+      }
+    }
   }
+
+  return msg;
 }
 
 module.exports = {
@@ -24,6 +57,7 @@ init: function(type, recv_func) {
   var handle_test_get = function() {
     var buf = new Buffer('testdata');
     return {
+      code: '2.04',
       encoding: 'text/plain;charset=utf-8',
       data: buf
     };
@@ -41,7 +75,8 @@ init: function(type, recv_func) {
   if(recv_func)
     callback_receive = recv_func;
 
-  return udp.init(type, this.recv);
+  svr_socket = udp.init(type, this.recv);
+  return svr_socket;
 },
 
 
@@ -64,10 +99,18 @@ recv: function(packet, peer) {
 
   connections.push(peer, msg);
 
-  handle_resource(peer, msg);
-
   if(this.callback_receive != undefined)
     this.callback_receive(msg, peer);
+
+  var ret_msg = handle_resource(peer, msg);
+
+  console.log('response: '+ret_msg.toString());
+
+  var ret_pkt = ret_msg.packetize();
+  svr_socket.send(ret_pkt, 0, ret_pkt.length, peer.port, peer.address,
+      function(err) {
+        if(err) console.log('error occurs: '+err);
+      });
 }
 
 }
